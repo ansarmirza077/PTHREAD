4420af4b542365e983317482da78c7e8ee57a739
Tej-Sample <polisettytejaswi11@gmail.com>
2022-08-10 18:29:25 +0530

    patt

---
+++ pattern3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#define limit 5
+int main()
+{
+	for(int i=limit;i>=0;i--)
+	{
+		
+		for(int j=limit;j>i;j--)
+	{
+		printf("%d",i);
+	}
+		printf("\n");
+	
+	}
+	return 0;
+}	
+
+
